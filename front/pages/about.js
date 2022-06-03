a236b8148e1b1292aa268d3b4c8824c2146ee337
Michał Nowotnik <micnowotnik@gmail.com>
2022-06-03 15:15:24 +0200

    pages

---
+++ front/pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>App with latest Shooting competitions on the world</p>
+            <p>Ver 1.0</p>
+        </div>
+    )
+}
